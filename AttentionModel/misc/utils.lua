ffa18744468c686014baa16b19f6d3c374e38747
Santhosh <santhoshkumar.25dec@gmail.com>
2016-12-02 09:56:24 +0530

    wrote data loading functions

--- AttentionModel/misc/utils.lua
+++ AttentionModel/misc/utils.lua
@@ -1,3 +1,4 @@
+local cjson = require 'cjson'
 local utils = {}
 
 function utils.getopt(opt, key, defaultValue)
@@ -6,6 +7,22 @@ function utils.getopt(opt, key, defaultValue)
 	end
 	if opt == nil or opt[key] == nil then return defaultValue end
 	return opt[key]
+end
+
+function utils.read_json(path)
+	local file = io.open(path, 'r')
+	local text = file:read()
+	file:close()
+	local info = cjson.decode(text)
+	reutrn info
+end
+
+function utils.count_keys(t)
+	local n = 0
+	for ,v in pairs(t) do
+		n = n + 1
+	end
+	return n
 end
 
 return utils
